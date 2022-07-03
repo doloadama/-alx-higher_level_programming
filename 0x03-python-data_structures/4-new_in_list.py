6eda877e955de8e5d6c2869041bf436a75675e82
Dolo Adama <doloadama@outlook.com>
2022-07-03 09:35:50 +0000

    UPDATED

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-
+    copie = my_list.copy()
+    
     if idx < 0 or idx > len(my_list):
-        return my_list
-    for i in range(len(my_list)):
-        if idx == i:
-            my_list[i] = element
-    return my_list
+        return copie
+    copie[idx] = element
+    return copie
