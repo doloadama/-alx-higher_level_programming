fc1e88a074b7c2eaf5176f3e6328e044a2c4eaab
doloadama <doloadama@outlook.com>
2022-07-20 21:50:40 +0000

    empty class Square that defines a square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
 
-"""
-0-square.py
-"""
+"""0-square.py"""
 
-""" 
-Define a new class 
-"""
-
-class square(self):
-    """ 
-    A simple class 
-    """
+class Square(self):
+"""Define a class """
     pass
