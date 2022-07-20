18a6b5c8c6269cd3aedb341bcebac08c12320478
doloadama <doloadama@outlook.com>
2022-07-20 22:16:19 +0000

    a class Square that defines a square by

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""a class Square that defines a square by"""
+
+class Square:
+    """Representation of a square"""
+    def __init__(self, size):
+        """The __init___ method initializes the size value of the square"""
+        self._Square__size = size
