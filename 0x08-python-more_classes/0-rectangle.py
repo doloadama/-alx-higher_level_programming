1641fed31385c3909c0c019bde4cc3717576799f
doloadama <doloadama@outlook.com>
2022-07-27 22:14:12 +0000

    An empty class Rectangle that defines a rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 0-rectangle.py
+
+"""
+Simple rectangle : An empty class Rectangle that defines a rectangle.
+"""
+
+
+class Rectangle:
+    """
+    An empty Rectangle class
+    """
+
+    pass
