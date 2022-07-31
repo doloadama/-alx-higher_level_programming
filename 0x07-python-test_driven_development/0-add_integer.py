71b4036f622d72cb05212e2ee86b1e851774c1f4
doloadama <doloadama@outlook.com>
2022-07-31 18:11:30 +0000

    Updated

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,10 +10,12 @@ these numbers can be integers or floats.
 def add_integer(a, b=98):
     """Returns the integer addition of a and b
     """
-if (type(a) is int or type(a) is float):
-        if (type(b) is int or type(b) is float):
-            return int(a) + int(b)
-        else:
-            raise TypeError("b must be an integer")
-    else:
+if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+    return a + b
