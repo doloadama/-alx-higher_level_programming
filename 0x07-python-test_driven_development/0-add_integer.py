2520bc9edfa361a848d572cff7aa38145d981a5b
doloadama <doloadama@outlook.com>
2022-07-31 17:55:23 +0000

    Updated

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#s#!/usr/bin/python3
 """Module containing simple function to add together two integers
 Must import this module to use it. Cannot be executed directly.
 To run doctests:
@@ -19,11 +19,11 @@ def add_integer(a, b):
         TypeError: If `type(a)` or `type(b)` is neither an int nor
         float.
 """
-                if type(a) is not int and type(a) is not float:
-                                raise TypeError('a must be an integer')
-                            if type(b) is not int and type(b) is not float:
-                                            raise TypeError('b must be an integer')
-                                        result = a + b
-                                            if result != result or result == float('inf') or result == -float('inf'):
-                                                            raise OverflowError
-                                                        return int(a) + int(b)
+   if type(a) is not int and type(a) is not float:
+      raise TypeError('a must be an integer')
+   if type(b) is not int and type(b) is not float:
+      raise TypeError('b must be an integer')
+   result = a + b
+   if result != result or result == float('inf') or result == -float('inf'):
+      raise OverflowError
+   return int(a) + int(b)
