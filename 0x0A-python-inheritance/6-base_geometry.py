43c50251265bf6171426d50d004be90af3d6b2a3
doloadama <doloadama@outlook.com>
2022-08-01 23:09:48 +0000

    UPDATED

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+"""Defines a base geometry class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent a base geometry"""
+
+    def area(self):
+        """area: return area"""
+        raise Exception('area() is not implemented')
