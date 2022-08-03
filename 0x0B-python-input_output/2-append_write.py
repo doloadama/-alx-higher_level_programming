8fa9b2bae9245eb69271a9eab4b44d45066f3fd4
doloadama <doloadama@outlook.com>
2022-08-03 13:33:53 +0000

    returns the number of characters added

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-
 """2-append_write.py"""
 
+
 def append_write(filename="", text=""):
-    """appends a string at the end of a text file (UTF8)
+    """Appends a string at the end of a text file (UTF8)
     Args:
-         filename: a text file
-         text: a string
+        filename: file to write to
+        text: text to write in in given file
     Return: the number of characters added
     """
-
     with open(filename, 'a', encoding="utf-8") as f:
-        return f.write(text)
+        count = f.write(text)
+
+    return count
