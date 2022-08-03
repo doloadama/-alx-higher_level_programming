1d0ba0d67ba28c7852dce29c45d26939e1eff6cb
doloadama <doloadama@outlook.com>
2022-08-03 12:27:15 +0000

    Defines a text file-reading function

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+function that reads a text file (UTF8) and prints it to stdout
+Prototype: def read_file(filename=""):
+You must use the with statement
+You don’t need to manage file permission or file doesn't exist exceptions.
+You are not allowed to import any module
+"""
+
+def read_file(filename=""):
+    """
+    reads a text file and prints it to stdout
+    """
+    with open('filename.txt', encoding="utf-8") as f:
+        print(f.read(), end="")
+    
