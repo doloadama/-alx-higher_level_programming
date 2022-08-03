d70874953f2631308d66912aea1efe8b7707d814
doloadama <doloadama@outlook.com>
2022-08-03 20:15:26 +0000

    class Student that defines a student by: (based on 9-student.py)

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-
-"""10-student.py"""
+"""Student to JSON"""
 
 
 class Student:
     """Defines a student"""
     def __init__(self, first_name, last_name, age):
-        """Initializes the first_name, last_name, age attributes"""
+        """Initializes the data"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-        
+
     def to_json(self, attrs=None):
-        """Retrieves a dictionary of a Student instances"""
+        """Returns a dictionary representation of a Student instance"""
         if attrs is None:
             return self.__dict__
         new_dict = {}
