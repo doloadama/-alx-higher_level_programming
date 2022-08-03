98a67680e07ee87a19630448d85153fa6e75e0cd
doloadama <doloadama@outlook.com>
2022-08-03 16:34:17 +0000

    class Student that defines a student by

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -6,7 +6,12 @@ class Student:
     """Defines a student"""
 
     def __init__(self, first_name, last_name, age):
-        """Initializes the date"""
+        """Initializes the date
+        Args:
+            first_name (str): first name
+            last_name (str: last name
+            age (int): age
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
