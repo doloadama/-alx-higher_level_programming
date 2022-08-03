7067213b0c2ab6005cb0f866bb6791e2d1b0a89e
doloadama <doloadama@outlook.com>
2022-08-03 15:48:50 +0000

    returns an object (Python data structure) represented by a JSON string

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -2,7 +2,7 @@
 """4-from_json_string.py"""
 import json
 
-def from_json_string(my_str):
-    """returns an object represented by a JSON string"""
-    return json.loads(my_str)
 
+def from_json_string(my_str):
+    """Returns an object represented by a JSON string"""
+    return json.loads(my_str)
