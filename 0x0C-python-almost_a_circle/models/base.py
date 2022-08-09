c69f0b79965016dd7da4a09d9d770c1f5fd620f6
doloadama <doloadama@outlook.com>
2022-08-09 22:47:01 +0000

    .

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,7 +24,6 @@ class Base(object):
     @staticmethod        
     def to_json_string(list_dictionaries):
         """ returns the JSON string representation of list_dictionaries"""
-        if list_dictionaries is None:
+        if list_dictionaries is None or len(list_dict) == 0:
             return "[]"
-        else:
-            return json.dumps(list_dictionaries)
+        return json.dumps(list_dictionaries)
