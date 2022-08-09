2c7ccc5997a2773f90129035db85543ec6ab4bd4
doloadama <doloadama@outlook.com>
2022-08-09 21:52:12 +0000

    A class that represents a square

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""square.py"""
+
+from models.rectangle import Rectangle
+
+class square(rectangle):
+    """A class that inherits the rectangle class"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Class constructor"""
+        super().__init__(id, x, y, size, size)
+        self.size = size
+
+    def __str__(self):
+        return '[Square] ({}) {}/{} - {}'.format(self.id, self.x, self.y,
+                                               self.size)
+
+    @property
+    def size(self):
+        """retrieve size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """set width and height with the same value, value"""
+        self.width = value
+        self.height = value
+        
