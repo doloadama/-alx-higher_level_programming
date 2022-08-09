67b32ccbd7947da67930f62bab0b28f99a74c3ad
doloadama <doloadama@outlook.com>
2022-08-09 16:05:32 +0000

    First

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -11,7 +11,7 @@ class Rectangle(Base):
     """
 
     def __init__(self, width: int, height: int, x=0, y=0, id=None):
-        """initialization
+        """class constructor
         """
         super().__init__(id)
 
@@ -20,6 +20,25 @@ class Rectangle(Base):
         self.x = x
         self.y = y
 
+
+    def __str__(self) -> str:
+        """string representation
+        """
+        return "[Rectangle] ({}) {}/{} - {}/{}" \
+            .format(self.id, self.x, self.y, self.width, self.height)
+
+    def check_type_value(self, name:  str, value: object, greater_equal=False):
+        """type and value validation
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if not greater_equal:
+            if value <= 0:
+                raise ValueError("{} must be > 0".format(name))
+        else:
+            if value < 0:
+                raise ValueError("{} must be >= 0".format(name))
 
     @property
     def width(self) -> int:
