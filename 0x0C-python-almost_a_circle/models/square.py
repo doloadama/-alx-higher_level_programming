cb665eaebaf079ec1f5fa8956f30e9e076825d57
doloadama <doloadama@outlook.com>
2022-08-09 22:00:25 +0000

    .

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -4,7 +4,7 @@
 
 from models.rectangle import Rectangle
 
-class square(rectangle):
+class Square(Rectangle):
     """A class that inherits the rectangle class"""
 
     def __init__(self, size, x=0, y=0, id=None):
