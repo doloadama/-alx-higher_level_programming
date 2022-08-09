cca2c7bd8d98fc000de687937cb446c8620e0d36
doloadama <doloadama@outlook.com>
2022-08-09 17:22:46 +0000

    seventh

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -101,3 +101,22 @@ class Rectangle(Base):
                                                        self.__y,
                                                        self.__width,
                                                        self.__height)
+
+    def update(self):
+        """print in stdout the Rectangle instance with the 
+        character # by taking care of x and y"""
+
+        if(args):
+            for i, j in enumerate(args):
+                if i == 0:
+                    self.id = j
+                elif i == 1:
+                    self.width = j
+                elif i == 2:
+                    self.height = j
+                elif i == 3:
+                    self.x = j
+                elif i == 4:
+                    self.y = j
+        for key, value in kwargs.items():
+            setattr(self, key, value)
