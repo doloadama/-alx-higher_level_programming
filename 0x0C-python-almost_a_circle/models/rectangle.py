4d0e9f353e6d96bc5e019e87ab09730971bf0817
doloadama <doloadama@outlook.com>
2022-08-09 17:11:29 +0000

    Fifth

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -83,3 +83,14 @@ class Rectangle(Base):
     def area(self):
         """returns the area value of rectangle"""
         return self.__width * self.__height
+
+    def display(self):
+        """print rectangle instance using # character"""
+        for line in range(self.__y):
+            print()
+        for i in range(self.__height):
+            for space in range(self.__x):
+                print(" ", end="")
+            for j in range(self.__width):
+                print("#", end="")
+            print()
