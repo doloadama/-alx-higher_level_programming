0180ec31d7a6f1ff487483d3016cbe64c1ed17b4
doloadama <doloadama@outlook.com>
2022-09-11 19:32:57 +0000

    .

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,6 +1,4 @@
--- Using the same data from ex.18
--- Display the max temperature of each state
-SELECT state, MAX(value) AS max_temp
-FROM temperatures
-GROUP BY state
-LIMIT 3;
+-- Display max temperature for each state
+SELECT `state`, MAX(`value`) AS `max_temp` FROM temperatures
+       GROUP BY `state`
+       ORDER BY `state` ASC;
