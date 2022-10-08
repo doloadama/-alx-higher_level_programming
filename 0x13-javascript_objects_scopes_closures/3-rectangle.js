8fd08f9ff96bafa024fe06367ab9cc6044c579c7
doloadama <doloadama@outlook.com>
2022-10-08 08:56:27 +0000

    .

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,22 @@
 #!/usr/bin/node
+class Rectangle{
+    constructor(w, h) {
+	if (typeof width === 'number' && typeof height === 'number' && width > 0 && height > 0) {
+	    this.width = w;
+	    this.height = w;
+	}
+    }
+    print () {
+	for (let i = 0; i < this.height; ++i) {
+	    let j = 0;
+	    for (; j < this.width; ++j) {
+		process.stdout.write('X');
+	    }
+
+	    if ( j === this.width) {
+		console.log('');
+	    }
+	}
+    }
+}
+module.export = Rectangle;
