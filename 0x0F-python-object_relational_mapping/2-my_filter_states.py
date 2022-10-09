18aa7d9fe6d0ae6cdd3d26e08e85825996605219
doloadama <doloadama@outlook.com>
2022-10-09 15:41:52 +0000

    .

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,7 +12,8 @@ if __name__ == '__main__':
     from the database.
     """
 
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+    db = MySQLdb.connect(host="localhost", user=argv[1],
+                         port=3306,
                          passwd=argv[2], db=argv[3])
 
     # Start cursor
