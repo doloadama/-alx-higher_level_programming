52a26229ab28fcd5b9372228241c20484a584c63
doloadama <doloadama@outlook.com>
2022-10-09 15:37:53 +0000

    .

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Displays all values in the states table of hbtn_0e_0_usa where
+name matches the argument"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """
+    Access to the database and get the states
+    from the database.
+    """
+
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
+
+    # Start cursor
+    cur = db.cursor()
+
+    #QUERY
+    cur.execute("SELECT *FROM states Where name LIKE BINARY '{}'\
+                 ORDER BY states.id ASC".format(argv[4]))
+    rows = cur.fetchall()
+
+    #PRINT QUERY
+
+    for row in rows:
+        print(row)
+
+    # Close cursor
+    cur.close()
+    db.close()
