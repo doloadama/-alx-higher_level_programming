0d49954f6be6fda6453dd5e08ad64eab62ca2b45
doloadama <doloadama@outlook.com>
2022-10-15 21:37:12 +0000

    .

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
-
-let count = 0;
+/* Function that prints the number of arguments already printed and the new
+ * argument value */
+let index = 0;
 exports.logMe = function (item) {
-  console.log(count + ':  ' + item);
-  count++;
+  console.log(`${index}: ${item}`);
+  index = index + 1;
 };
