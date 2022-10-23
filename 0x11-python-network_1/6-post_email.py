f5b0fff6769657e9ba9b41890892d935a874fece
doloadama <doloadama@outlook.com>
2022-10-23 10:02:33 +0000

    .

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
-"""Takes in a URL and an email address, sends a
-POST request to the passed URL with the email
-as a parameter, and finally displays the body
-of the response.
 """
-
-from sys import argv
+Script that takes in a URL and an email address, sends a POST request to the passed URL
+with the email as a parameter, and finally displays the body of the response
+"""
 import requests
+from sys import argv
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     payload = {'email': argv[2]}
-    req = requests.post(argv[1], data=payload)
-
-    print(req.text)
+    r = requests.post(argv[1], data=payload)
+    print(r.text)
