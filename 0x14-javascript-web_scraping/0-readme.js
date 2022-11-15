83d57b22d39babac102b0aa100863292523aa59a
doloadama <doloadama@outlook.com>
2022-11-15 19:32:38 +0000

    .

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
+// A script that reads and prints the content of a file
+
+const file = process.argv[2];
 const fs = require('fs');
 
-if (process.argv.length > 2) {
-    fs.readFile(process.argv[2], (err, data) => {
-	if (err) {
-	    console.log(err);
-	} else {
-	    console.log(data.toString('utf-8'));
-	}
-    });
-}
+fs.readFile(file, 'utf8', (error, data) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
+});
