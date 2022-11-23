5b1434e880fc531292df20724b77276f4cb96fb1
doloadama <doloadama@outlook.com>
2022-11-23 21:31:27 +0000

    .

--- 0-script.js
+++ 0-script.js
@@ -1,2 +1,4 @@
-// updates the text color of the HTML tag HEADER to red
-document.querySelector('header').style.color = '#FF0000';
+// Javascript script that updates the text color of the <header> element to red (#FF0000)
+
+const colorH = document.querySelector('header');
+colorH.style.color = '#FF0000';
